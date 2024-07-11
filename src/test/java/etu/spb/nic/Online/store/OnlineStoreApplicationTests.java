fd5bfc1501e928ac860beabb20449f4af918bf09
dborodulin <maximus906544@yandex.ru>
2024-07-11 15:31:38 +0300

    init project online store

---
+++ src/test/java/etu/spb/nic/Online/store/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package etu.spb.nic.Online.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
